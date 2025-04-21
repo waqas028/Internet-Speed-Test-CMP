60702d1d50a7b5dfe5cba711f7bf127cba61eeeb
farimarwat <farimarwat@gmail.com>
2025-04-21 16:07:03 +0500

    Add security exception in iOS
    http request was not allowed. So exception added in plist.

--- composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/utils/UrlUtils.ios.kt
+++ composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/utils/UrlUtils.ios.kt
@@ -1,15 +1,16 @@
 package com.farimarwat.speedtest.utils
 
 import platform.Foundation.NSCharacterSet
+import platform.Foundation.NSMutableCharacterSet
 import platform.Foundation.NSString
 import platform.Foundation.URLQueryAllowedCharacterSet
 import platform.Foundation.stringByAddingPercentEncodingWithAllowedCharacters
 import platform.Foundation.stringByRemovingPercentEncoding
 
 actual fun String.encodeUrl(): String {
-    return (this as NSString).stringByAddingPercentEncodingWithAllowedCharacters(
-        NSCharacterSet.URLQueryAllowedCharacterSet
-    ) ?: this
+    val allowed = NSMutableCharacterSet.alphanumericCharacterSet()
+    allowed.addCharactersInString("-._~") // Same as Android's URLEncoder
+    return (this as NSString).stringByAddingPercentEncodingWithAllowedCharacters(allowed) ?: this
 }
 
 actual fun String.decodeUrl(): String {
