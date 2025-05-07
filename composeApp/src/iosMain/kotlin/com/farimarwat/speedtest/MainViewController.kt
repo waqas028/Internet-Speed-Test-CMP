5fbf5731a78db9f3d7890eeba0c7d942174b60c4
farimarwat <farimarwat@gmail.com>
2025-05-07 07:40:48 +0500

    Add iOS side testlocations map view

--- composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.farimarwat.speedtest
 
 import androidx.compose.ui.window.ComposeUIViewController
+import cocoapods.GoogleMaps.GMSServices
 import cocoapods.Google_Mobile_Ads_SDK.GADMobileAds
 import com.farimarwat.speedtest.di.initKoin
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -10,5 +11,6 @@ fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin()
         GADMobileAds.sharedInstance().startWithCompletionHandler(null)
+        GMSServices.provideAPIKey("AIzaSyAUiMDU2heGnHFz2B9IMDuDoBxS-7XzRfc")
     }
 ) { App() }
--- composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/feature/map/TestLocations.ios.kt
+++ composeApp/src/iosMain/kotlin/com/farimarwat/speedtest/feature/map/TestLocations.ios.kt
@@ -1,10 +1,84 @@
 package com.farimarwat.speedtest.feature.map
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asSkiaBitmap
+import androidx.compose.ui.viewinterop.UIKitView
+import cocoapods.GoogleMaps.GMSCameraPosition
+import cocoapods.GoogleMaps.GMSCameraUpdate
+import cocoapods.GoogleMaps.GMSMapView
+import cocoapods.GoogleMaps.GMSMarker
+import cocoapods.GoogleMaps.GMSMutablePath
+import cocoapods.GoogleMaps.GMSPolyline
 import com.farimarwat.speedtest.domain.model.SpeedTest
+import com.farimarwat.speedtest.feature.ad.toUIColor
+import kotlinx.cinterop.ExperimentalForeignApi
+import org.jetbrains.compose.resources.painterResource
+import platform.CoreGraphics.CGRectMake
+import platform.CoreGraphics.CGSizeMake
+import platform.CoreLocation.CLLocationCoordinate2D
+import platform.CoreLocation.CLLocationCoordinate2DMake
+import platform.UIKit.UIColor
+import platform.UIKit.UIGraphicsImageRenderer
+import platform.UIKit.UIImage
+import platform.UIKit.systemBlueColor
+import speedtest.composeapp.generated.resources.Res
+import speedtest.composeapp.generated.resources.wifi
 
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun TestLocations(test: SpeedTest) {
+    val map = remember { GMSMapView() }
 
+    val providerPosition = CLLocationCoordinate2DMake(
+        latitude = test.providerLat,
+        longitude = test.providerLon
+    )
+    val serverPosition = CLLocationCoordinate2DMake(
+        latitude = test.serverLat,
+        longitude = test.serverLon
+    )
+
+    val cameraPosition = GMSCameraPosition.cameraWithLatitude(
+        latitude = test.providerLat,
+        longitude = test.providerLon,
+        zoom = 10f
+    )
+    val cameraUpdate = GMSCameraUpdate.setCamera(cameraPosition)
+    map.moveCamera(cameraUpdate)
+
+    GMSMarker().apply {
+        position = providerPosition
+        title = "Provider: ${test.providerName}"
+        snippet = test.providerName
+        icon = UIImage.imageNamed("wifi")
+        tracksInfoWindowChanges = true
+        setMap(map)
+    }
+    GMSMarker().apply {
+        position = serverPosition
+        title = "Server: ${test.serverName}"
+        icon = UIImage.imageNamed("server")
+        setMap(map)
+    }
+    val path = GMSMutablePath().apply {
+        addCoordinate(providerPosition)
+        addCoordinate(serverPosition)
+    }
+
+    GMSPolyline().apply {
+        this.path = path
+        strokeColor = MaterialTheme.colorScheme.background.toUIColor()
+        strokeWidth = 10.0
+        setMap(map)
+    }
+    UIKitView(
+        factory = { map },
+        modifier = Modifier.fillMaxSize()
+    )
 }
